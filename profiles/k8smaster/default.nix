962ebb3b14c5f6a9a05428edd683837c30af5fed
samrose <samuel.rose@gmail.com>
2019-09-12 13:44:40 -0400

    env var

---
+++ profiles/k8smaster/default.nix
@@ -0,0 +1,74 @@
+{ config, lib, pkgs, ... }:
+
+with pkgs;
+
+let
+  inherit (config.system.example) target;
+in
+
+{
+  imports = [
+    ../.
+    ../self-aware.nix
+  ];
+
+
+  documentation.enable = false;
+
+  environment.noXlibs = true;
+
+  environment.systemPackages = [
+    vim
+    git
+    kubectl
+    kompose    
+  ]; 
+  
+  environment.variables.KUBECONFIG = "/etc/kubernetes/cluster-admin.kubeconfig";
+  
+  services.kubernetes = {
+      roles = ["master" "node"];
+      masterAddress = "localhost";
+  };
+  networking.firewall.allowedTCPPorts = [ 8080 ];
+  networking.hostName = lib.mkOverride 1100 "example";
+
+  nix.binaryCaches = [
+    "https://cache.nixos.org"
+  ];
+
+
+  nix.gc = {
+    automatic = true;
+    dates = "daily";
+    options = "--delete-older-than 7d";
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  services.avahi = {
+    enable = true;
+
+    publish = {
+      enable = true;
+      addresses = true;
+    };
+  };
+ 
+  services.mingetty.autologinUser = "root";
+
+  services.openssh.enable = true;
+  
+  system.example.autoUpgrade = {
+    enable = true;
+    dates = "*:0/01";
+  };
+
+  system.stateVersion = "19.09";
+
+  users.mutableUsers = false;
+
+
+  users.users.root.hashedPassword = "*";
+
+}
